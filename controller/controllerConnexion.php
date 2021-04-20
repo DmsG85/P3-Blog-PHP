48ab99a9c5b65606411f7664fff82245c66f190c
DmsG85 <dmsgaelle@gmail.com>
2021-04-20 14:36:13 +0200

    add function registeruser

--- controller/controllerConnexion.php
+++ controller/controllerConnexion.php
@@ -7,21 +7,21 @@ public function loginuser(){
         if(isset($_POST["connexion"])&& $_POST["connexion"] === "ok") {
                 $email=$_POST["email"];
                 $password=$_POST["password"];
-                // var_dump($email);
-                // var_dump($password);
+                var_dump($email);
+                var_dump($password);
                 $user=new Users;
                 $verif=$user->verifemail($email);
-                // var_dump($verif);
+                var_dump($verif);
                 if ($verif != null){
                        if ($password===$verif["password"]){
                                 $_SESSION["login"]=$verif["idUser"];
                        }
                        else{
-                               $error='mot de passe incorrecte';
+                               $error='Votre mot de passe est incorrecte, veuillez recommencer.';
                        }
                 }
                 else {
-                        $error='adresse invalide';
+                        $error='Votre adresse est invalide, veuillez recommencer.';
                 }
                 if (isset ($error)){
                         
@@ -31,40 +31,41 @@ public function loginuser(){
         }
         
 }
-public function registeruser(){
+// public function registeruser(){
 
-        if(isset($_POST["register"])) {
-                $pseudo=$_POST["pseudo"];
-                $email=$_POST["email"];
-                $password=$_POST["password"];
-                $cpassword=$_POST["cpassword"];
-                var_dump($email);
-                var_dump($password);
-                var_dump($pseudo);
-                var_dump($cpassword);
-                $user=new Users;
-                // $verif=$user->verifemail($email);
-                // var_dump($verif);
-                if($password == $cpassword){
+//         if(isset($_POST["register"])) {
+//                 $pseudo=$_POST["pseudo"];
+//                 $email=$_POST["email"];
+//                 $password=$_POST["password"];
+//                 $cpassword=$_POST["cpassword"];
+//                 var_dump($email);
+//                 var_dump($password);
+//                 var_dump($pseudo);
+//                 var_dump($cpassword);
+//                 $user=new Users;
+               
+//                 if($password === $cpassword){
 
-                }else $return = "Les deux mots de passe ne correspondent pas.";
-                if ($verif != null){
-                       if ($password===$verif["password"]){
-                                $_SESSION["register"]=$verif["idUser"];
-                       }
-                       else{
-                               $error='mot de passe incorrecte';
-                       }
-                }
-                else {
-                        $error='adresse invalide';
-                }
-                if (isset ($error)){
+//                 }else{
+//                         $return = "Les deux mots de passe ne correspondent pas.";
+//                 } 
+//                 if ($verif != null){
+//                        if ($password===$verif["password"]){
+//                                 $_SESSION["register"]=$verif["idUser"];
+//                        }
+//                        else{
+//                                $error='mot de passe incorrecte';
+//                        }
+//                 }
+//                 else {
+//                         $error='adresse invalide';
+//                 }
+//                 if (isset ($error)){
                         
-                        return $error;
-                }
+//                         return $error;
+//                 }
                 
-        }
-}
+//         }
+// }
 }
 ?>
